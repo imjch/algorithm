da78edc758aeab3dd26cd921b07fa3248811ea37
imjch <imjch@hotmail.com>
2014-06-02 00:35:23 +0800

    changed the structure

--- quick_pow/pow(iterasive).cpp
+++ quick_pow/pow(iterasive).cpp
@@ -1,22 +1,3 @@
-//version of recursion
-
-BigInteger pow(BigInteger x,int N)
-{
-    if (N==1){
-         return x;
-    }
-    if (N==0){
-         return 1;
-    }
-    if (N%2==0) {
-         return pow(x*x,N>>=2);
-    }
-    else {
-         return x*pow(x*x, N>>=/2);
-    }
-}
-
-//version of iteration
 BigInteger quick_pow(BigInteger x,int N)
 {
     //boundary condition
